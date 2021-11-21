003e7e5b10e3e34d2e58d59492e34b5c1a511953
yohanan400 <41789360+yohanan400@users.noreply.github.com>
2021-11-21 18:56:56 +0200

    Delete EX6/EX6-2/DoxygenDocs/html directory

--- EX6/EX6-2/DoxygenDocs/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ex6_2d2_2eh_0',['EX6-2.h',['../_e_x6-2_8h.html',1,'']]]
-];
--- EX6/EX6-2/DoxygenDocs/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['charprinthex_0',['CharPrintHex',['../_e_x6-2_8h.html#a206fd97b3e90ad7337a60eef7b2e11f8',1,'EX6-2.c']]]
-];
